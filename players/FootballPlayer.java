bb6a18cb588ca32ecd4dcae9eadd7cc9d9c76553
Pankhuri Gupta <pankhurig1@gmail.com>
2023-03-05 18:10:34 +0530

    Add files via upload

---
+++ players/FootballPlayer.java
@@ -0,0 +1,11 @@
+package tim.arraylist.generics.players;
+
+/**
+ * Created by dev on 17/10/2015.
+ */
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
