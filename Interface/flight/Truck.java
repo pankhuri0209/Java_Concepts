7bde331e5dc59934bec5caf719e87f72c817c759
Pankhuri Gupta <pankhurig1@gmail.com>
2023-03-05 10:43:11 +0530

    Add files via upload

---
+++ Interface/flight/Truck.java
@@ -0,0 +1,9 @@
+package tim.arraylist.Interface.flight;
+
+public class Truck implements Trackable {
+
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName() + "'s coordinates recorded");
+    }
+}
